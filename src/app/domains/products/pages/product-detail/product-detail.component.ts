0dfda76c78772577b9c06478ad1684a5eeb5db15
Juan Pablo Espinosa Piedad <jp.espinosa.piedad@hotmail.com>
2026-09-19 09:55:13 -0600

    Clase 13: linkedSignal en Angular: computed con set

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,4 +1,11 @@
-import { Component, inject, signal, OnInit, input } from '@angular/core';
+import {
+  Component,
+  inject,
+  signal,
+  OnInit,
+  input,
+  linkedSignal,
+} from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { ProductService } from '@shared/services/product.service';
 import { Product } from '@shared/models/product.model';
@@ -11,31 +18,60 @@ import { CartService } from '@shared/services/cart.service';
 })
 export default class ProductDetailComponent implements OnInit {
   readonly slug = input<string>();
-  product = signal<Product | null>(null);
-  cover = signal('');
+  $product = signal<Product | null>(null);
+  $cover = linkedSignal({
+    source: this.$product,
+    computation: (product) => {
+      // computation: (product, previousValue)
+      return product && product.images.length > 0 ? product.images[0] : '';
+    },
+  });
+
+  /*
+  $cover = linkedSignal(() => {
+    const product = this.$product();
+    return product && product.images.length > 0 ? product.images[0] : '';
+  });
+  */
+
+  //cover = signal('');
+
+  /*
+  cover = computed(() => {
+    const product = this.product();
+    return product && product.images.length > 0 ? product.images[0] : '';
+  });
+  */
+
   private productService = inject(ProductService);
   private cartService = inject(CartService);
+
+  /*
+  constructor() {
+    const product = this.product();
+    if (product && product.images.length > 0) {
+      this.cover.set(product.images[0]);
+    }
+  }
+  */
 
   ngOnInit() {
     const slug = this.slug();
     if (slug) {
       this.productService.getOne({ product_slug: slug }).subscribe({
         next: (product) => {
-          this.product.set(product);
-          if (product.images.length > 0) {
-            this.cover.set(product.images[0]);
-          }
+          this.$product.set(product);
         },
       });
     }
   }
 
   changeCover(newImg: string) {
-    this.cover.set(newImg);
+    this.$cover.set(newImg);
   }
 
   addToCart() {
-    const product = this.product();
+    const product = this.$product();
     if (product) {
       this.cartService.addToCart(product);
     }
