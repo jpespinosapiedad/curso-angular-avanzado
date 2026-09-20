258b62a6055e91087312ca15a0f3841216b3d11e
Juan Pablo Espinosa Piedad <jp.espinosa.piedad@hotmail.com>
2026-09-20 11:05:19 -0600

    Clase 15: toSignal: Observables de RxJS como Signals

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -6,6 +6,8 @@ import { HighlightDirective } from '@shared/directives/highlight.directive';
 
 import { WaveAudioComponent } from '@info/components/wave-audio/wave-audio.component';
 import { FormsModule } from '@angular/forms';
+import { BehaviorSubject, delay, Subject } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-about',
@@ -22,6 +24,16 @@ export default class AboutComponent {
   duration = signal(1000);
   message = signal('Hola');
 
+  obsWithInit$ = new BehaviorSubject<string>('obsWithInit$ initial value');
+  $withInit = toSignal(this.obsWithInit$, {
+    requireSync: true,
+  });
+
+  obsWithoutInit$ = new Subject<string>();
+  $withoutInit = toSignal(this.obsWithoutInit$.pipe(delay(3000)), {
+    initialValue: 'obsWithoutInit$ initial value',
+  });
+
   changeDuration(event: Event) {
     const input = event.target as HTMLInputElement;
     this.duration.set(input.valueAsNumber);
@@ -29,5 +41,13 @@ export default class AboutComponent {
 
   changeMessage(event: string) {
     console.log('change message', event);
+  }
+
+  emitWithInit() {
+    this.obsWithInit$.next('obsWithInit$ new value');
+  }
+
+  emitWithoutInit() {
+    this.obsWithoutInit$.next('obsWithoutInit$ new value');
   }
 }
