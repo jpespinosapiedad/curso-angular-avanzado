b75b657e4b8c5f3cf77a26a886de1f9263beaf17
Juan Pablo Espinosa Piedad <jp.espinosa.piedad@hotmail.com>
2026-09-20 11:17:23 -0600

    Clase 16: Conversión de Observables a Signals en Angular con toSignal

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,11 +1,4 @@
-import {
-  Component,
-  inject,
-  signal,
-  OnInit,
-  OnChanges,
-  input
-} from '@angular/core';
+import { Component, inject, signal, OnChanges, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLinkWithHref } from '@angular/router';
 import { ProductComponent } from '@products/components/product/product.component';
@@ -14,24 +7,24 @@ import { Product } from '@shared/models/product.model';
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
 import { CategoryService } from '@shared/services/category.service';
-import { Category } from '@shared/models/category.model';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-list',
   imports: [CommonModule, ProductComponent, RouterLinkWithHref],
   templateUrl: './list.component.html',
 })
-export default class ListComponent implements OnInit, OnChanges {
-  products = signal<Product[]>([]);
-  categories = signal<Category[]>([]);
+export default class ListComponent implements OnChanges {
   private cartService = inject(CartService);
   private productService = inject(ProductService);
   private categoryService = inject(CategoryService);
+
   readonly slug = input<string>();
 
-  ngOnInit() {
-    this.getCategories();
-  }
+  products = signal<Product[]>([]);
+  $categories = toSignal(this.categoryService.getAll(), {
+    initialValue: [],
+  });
 
   ngOnChanges() {
     this.getProducts();
@@ -45,14 +38,6 @@ export default class ListComponent implements OnInit, OnChanges {
     this.productService.getProducts({ category_slug: this.slug() }).subscribe({
       next: (products) => {
         this.products.set(products);
-      },
-    });
-  }
-
-  private getCategories() {
-    this.categoryService.getAll().subscribe({
-      next: (data) => {
-        this.categories.set(data);
       },
     });
   }
